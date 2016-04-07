42d435b94b5c5f109ef9f61f1b272d79cbe4c8a8
Daniel Levy <dlevy@us.ibm.com>
2016-04-07 09:55:02 -0500

    Get rid of second add for vxlan. Fix issue with retrieval of vxlan list

--- deploy_fdbinsert.sh
+++ deploy_fdbinsert.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-VXLAN_LIST=$(brctl show | grep vxlan | sed -e 's/^[ \t]*//')
+VXLAN_LIST=$(ip a | grep vxlan | awk '{print $2}')
 
 for VXLAN in $VXLAN_LIST
 do
     bridge fdb add 00:00:00:00:00:00 dev $VXLAN dst 10.130.101.139 self permanent
     bridge fdb append 00:00:00:00:00:00 dev $VXLAN dst 10.130.101.139 self permanent
-    bridge fdb add 00:00:00:00:00:00 dev $VXLAN dst 10.130.101.138 self permanent
     bridge fdb append 00:00:00:00:00:00 dev $VXLAN dst 10.130.101.138 self permanent
 done
