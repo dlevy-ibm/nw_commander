8f8c1f473cf6b0e1703ecc1cc20fe1b37dbd1360
Daniel Levy <dlevy@us.ibm.com>
2016-04-10 13:02:36 -0500

    Update the netperf test code

--- deploy_netperf.sh
+++ deploy_netperf.sh
@@ -13,8 +13,8 @@ netperftest() {
     wait $!
     echo "----------------" >> ~/$2
     echo netperf process has completed
-
-
+    
+    
     # collect data after
     echo collect packet data after test... 
     echo "Packet data before" >> ~/$2
@@ -23,7 +23,8 @@ netperftest() {
     echo "----------------" >> ~/$2
 }
 
+netperftest REPLACE_INPUT
 
-netperftest TCP_STREAM netperftcp.txt
-netperftest UDP_STREAM netperfudp.txt "-R 1"
-netperftest TCP_RR netperftcprr.txt
+#netperftest TCP_STREAM netperftcp.txt
+#netperftest UDP_STREAM netperfudp.txt "-R 1"
+#netperftest TCP_RR netperftcprr.txt
